d688d1441fd5ace23ef51c9ba004289dfb41d081
Kai Schaper <386066+kschaper@users.noreply.github.com>
2018-08-17 19:10:17 +0200

    wip: add minimal-setup-with-jsx

---
+++ minimal-setup-with-jsx/README.md
@@ -0,0 +1,3 @@
+# Minimal React Setup (with JSX)
+
+See https://kschaper.gitlab.io/posts/minimal-react-setup-with-jsx/
---
+++ minimal-setup-with-jsx/webpack.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['babel-plugin-transform-react-jsx']
+          }
+        }
+      }
+    ]
+  }
+}
